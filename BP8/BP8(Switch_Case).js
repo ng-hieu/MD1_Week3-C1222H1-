587413ce1021caabf85bd3d582ef3b0c1862aea8
Your Name <you@example.com>
2023-01-09 09:56:50 +0700

    switch_case

---
+++ BP8/BP8(Switch_Case).js
@@ -0,0 +1,31 @@
+function check () {
+let browser = document.getElementById("browser").value;
+let a = "Okay we support these browsers too" ;
+let b = "We hope that this page looks ok";
+let c = "You've got the Edge!";
+ switch (browser) {
+     case "Edge":
+         document.getElementById("result").innerHTML = c;
+         break;
+     case "Chrome":
+     case "Firefox":
+     case "Safari":
+     case "Opera":
+         document.getElementById("result").innerHTML = a;
+         break;
+     default:
+         document.getElementById("result").innerHTML = b;
+         break;
+ }
+}
+let a = +prompt ("a?", '');
+switch (a){
+    case 0: alert("0");
+        break;
+    case 1: alert("1");
+        break;
+    case 2:
+    case 3:
+        alert("2, 3");
+        break;
+}
